b202f558d955b2d8a75a2c7c3868a40d57132b37
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2018-08-24 18:35:27 -0400

    psuedo code for js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,20 @@
-// need to psuedo code the events for the game //
+// Crystal Collector //
+
+//Make four buttons with on click event 
+
+//player is shown random number at start of every game
+
+//make random number generator for number for 4 crystals and number for player to match
+
+// on click, random number is added to players total score (ON CLICK UPDATE PLAYERS SCORE)
+
+//add (win to win) or (loss to loss) if player matches/does not match random number from start of game
+
+// he game automatically restarts when player wins or loses
+
+//make sure random number shows to player at start of each game and crystals should have new random numbers on click
+
+//Keep random number shown to player between 19-120 and each crystal should have hidden value between 1-12
+
+
 
