378e2c966c53ec8b4aad2c2f0a762c7a4c0c107a
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2018-08-25 12:42:34 -0400

    deleted psuedo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -61,16 +61,13 @@ $(document).ready(function() {
 
 });
 
-//make random number generator for number for 4 crystals and number for player to match
-
-
-// on click, random number is added to players total score (ON CLICK UPDATE PLAYERS SCORE)
 
 //add (win to win) or (loss to loss) if player matches/does not match random number from start of game
 
-// he game automatically restarts when player wins or loses
 
-//make sure random number shows to player at start of each game and crystals should have new random numbers on click
+// The game automatically restarts when player wins or loses
 
-//Keep random number shown to player between 19-120 and each crystal should have hidden value between 1-12
+
+
+
 
